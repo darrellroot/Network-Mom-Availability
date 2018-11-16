969414b5c1df4b51f493f0d950d923b3fc204b94
Darrell Root <darrellrootjunk@mac.com>
2018-11-16 14:03:07 -0800

    Initial Commit

---
+++ Network Mom/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Network Mom
+//
+//  Created by Darrell Root on 11/16/18.
+//  Copyright © 2018 Darrell Root LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
