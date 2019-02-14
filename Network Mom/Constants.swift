f2859c329ec9c6774b5fb6df61ce296ea29adc59
Darrell Root <darrellrootjunk@mac.com>
2019-02-13 17:22:46 -0800

    1.0 build 2 release version uploaded to app store

--- Network Mom/Constants.swift
+++ Network Mom/Constants.swift
@@ -21,10 +21,10 @@ struct Constants {
     static let CoreLicense = "CoreLicense"
     static let GracePeriod = "GracePeriod"
     static let gracePeriodTransaction = "100"
-    static let gracePeriodDuration = 1800.0
-    //static let GraceLicenseDuration = 3600.0 * 24.0 * 31.0
-    static let annualLicenseDuration = 3600.0 * 1.0 * 1.0
-    //static let annualLicenseDuration = 3600.0 * 24.0 * 366.0
+    //static let gracePeriodDuration = 1800.0
+    static let gracePeriodDuration = 3600.0 * 24.0 * 31.0
+    //static let annualLicenseDuration = 3600.0 * 1.0 * 1.0
+    static let annualLicenseDuration = 3600.0 * 24.0 * 366.0
     static let CoreMap = "CoreMap"
     static let CoreMonitorIPv4 = "CoreMonitorIPv4"
     static let CoreMonitorIPv6 = "CoreMonitorIPv6"
